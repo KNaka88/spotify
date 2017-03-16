fc88fd5b80d3d38ade832ec605c07dd5fdeb964a
Koji Nakagawa and Katie Reed <koji@>
2017-03-16 16:22:11 -0700

    add styling, add album tracks list and preview with modal views

--- js/music-interface.js
+++ js/music-interface.js
@@ -1,7 +1,12 @@
 var Artist = require('./../js/music.js').artistModule;
 
-var displayArtist = function(artistName, displayImages, artistId, album_names, album_images, top_track_names, top_track_previews) {
-  $('#artist_image').append("<img src='" + displayImages + "'>");
+var displayArtist = function(artistName, displayImages, artistId, album_names, album_images, album_ids, top_track_names, top_track_previews) {
+  $('#artist_image').empty();
+  $('#artist_name').empty();
+  $('#album_names').empty();
+  $('#top_tracks').empty();
+  $('#top_track_previews').empty();
+  $('#artist_image').append("<img id='top_artist_img' src='" + displayImages + "'>");
   $('#artist_name').text(artistName);
 
   for(var i=0; i<album_names.length; i++){
@@ -9,28 +14,20 @@ var displayArtist = function(artistName, displayImages, artistId, album_names, a
       "<div class='col-md-4'>"+
         "<div class='card' style='width: 320px;'>" +
           "<h4 class='card-header'>" +
-          "<div class='album-title'>" +
-            album_names[i] +
-          "</div>" +
+            "<div class='album-title'>" +
+              album_names[i] +
+            "</div>" +
            "</h4>" +
           "<div class='card-block'>" +
-            "<img class='card-img-top' src='" + album_images[i]+ "'alt='album cover'>" +
+            "<img id='"+album_ids[i]+"'class='card-img-top album' src='" + album_images[i]+ "'alt='album cover' type='button' data-toggle='modal' data-target='#trackModal'>" +
           "</div>" +
         "</div>" +
       "</div>"
     );
-    // $('#album_names').append("<li>" + album_names[i] + "</li>");
-    // $('#album_images').append("<img src='" + album_images[i] + "'>");
   }
 
+  $('#top_tracks').append("<h3 id='top-tracks-heading'>Top Tracks</h3>");
   for(var j=0; j<top_track_names.length; j++){
-    // $('#top_tracks').append("<li> "+top_track_names[j] + "</li>"
-    //   );
-    //
-    // $('#tracks_samples').append(" <audio controls=controls>" +
-    // "<source src='"+top_track_previews[j]+"'type='audio/mpeg'>" +
-    // "</audio></li>");
-
     $('#top_tracks').append(
       "<div class='col-md-7 track_names'>" +
         "<li>"+ top_track_names[j] + "</li>" +
@@ -44,20 +41,47 @@ var displayArtist = function(artistName, displayImages, artistId, album_names, a
 
   }
 
+  addClicks();
+};
 
+var addClicks = function() {
+  $('.album').click(function() {
+    console.log("clicked!");
+    var id = $(this).attr("id");
+    var albumTitle = $(this).parent().siblings().find('.album-title').text();
+    console.log(albumTitle);
+    var newArtist  = new Artist();
+    newArtist.getTracks(id,displayAlbum, albumTitle);
+  });
+};
 
+var displayAlbum = function(tracks, albumTitle) {
+  var html = "";
+  for(var i=0; i<tracks.length; i++){
+    html += "<div class='col-md-7'><li>" + tracks[i][1] + "</li></div>";
+    html += "<div class='col-md-5'><li><audio controls=controls>" +
+              "<source src='" + tracks[i][2] + "'type='audio/mpeg'>" +
+              "</audio></li></div>";
+  }
+  $("#album_tracks").html(html);
+  $('.modal-title').text(albumTitle);
 };
 
 
 $(document).ready(function(){
   $("#artist_form").submit(function(event){
     event.preventDefault();
+
     var userInput = $("#artist").val();
 
     var newArtist  = new Artist();
     newArtist.showArtist(userInput, displayArtist);
 
   });
+
+
+
+
 });
 
 
--- js/music.js
+++ js/music.js
@@ -10,8 +10,10 @@ Artist.prototype.showArtist = function(userInput, callback){
   var artistId = "";
   var album_names = [];
   var album_images = [];
+  var album_ids = [];
   var top_track_names = [];
   var top_track_previews = [];
+
 
 
   $.get("https://api.spotify.com/v1/search?q="+userInput+"&type=artist")
@@ -27,6 +29,8 @@ Artist.prototype.showArtist = function(userInput, callback){
       for(var i=0; i<response2.items.length; i++){
           album_names.push(response2.items[i].name);
           album_images.push(response2.items[i].images[1].url);
+          album_ids.push(response2.items[i].id);
+
       }
       return $.get("https://api.spotify.com/v1/artists/"+artistId+"/top-tracks?country=US");
     })
@@ -39,8 +43,25 @@ Artist.prototype.showArtist = function(userInput, callback){
     })
 
     .then(function(){
-      callback(artistName, displayImages, artistId, album_names, album_images, top_track_names, top_track_previews);
+      callback(artistName, displayImages, artistId, album_names, album_images, album_ids, top_track_names, top_track_previews);
     });
 
+
 };
+
+Artist.prototype.getTracks = function(id, callback, albumTitle) {
+  var tracks = [];
+  $.get("https://api.spotify.com/v1/albums/"+id+"/tracks?market=US")
+    .then(function(response){
+      console.log(response);
+      for(var i=0; i<response.items.length; i++ ){
+        var track = [response.items[i].track_number, response.items[i].name, response.items[i].preview_url];
+        tracks.push(track);
+      }
+
+      callback(tracks,albumTitle);
+
+    });
+};
+
 exports.artistModule = Artist;
